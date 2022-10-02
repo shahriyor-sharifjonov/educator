d8aad4201808744133c02c1d5cb5a399ac0bcec2
Shahriyor - Keypix <78787657+shahriyor-sharifjonov@users.noreply.github.com>
2022-10-02 15:53:39 +0500

    initial

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,16 @@ import * as functions from "./modules/functions.js";
 
 functions.isWebp();
 
-// import Swiper, { Navigation, Pagination } from 'swiper';
+import Swiper, { Navigation, Pagination } from 'swiper';
 
-// const swiper = new Swiper();
+const swiper = new Swiper();
+
+new Swiper('.courses__swiper', {
+    modules: [Navigation],
+    slidesPerView: 4,
+    spaceBetween: 24,
+    navigation: {
+        nextEl: '.courses__next',
+        prevEl: '.courses__prev',
+    },
+});
